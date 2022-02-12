a5b72b7de07e522821a12a5f3f6d02d4a904ad7f
josem <josem@DESKTOP-NHRJS3T>
2022-02-12 20:23:57 +0100

    Prueba de versiones

---
+++ src/cuentas/Main.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cuentas;
+
+/**
+ *
+ * @author josem
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        operativa_cuenta(0);
+    }
+
+    private static void operativa_cuenta(float cantidad) {
+        CCuenta Cuenta1;
+        
+        double saldoActual;
+        
+        Cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = Cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+
+        try {
+            Cuenta1.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            Cuenta1.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+    
+}
